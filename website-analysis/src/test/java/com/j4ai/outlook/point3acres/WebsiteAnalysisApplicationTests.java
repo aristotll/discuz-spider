c5243d8970ea8ab55fef7f43dba452f653df9c7a
Tony <sinoxutong@live.cn>
2017-10-28 09:12:52 +0800

    初始化分析程序
    Spring Boot 生成器生成

---
+++ website-analysis/src/test/java/com/j4ai/outlook/point3acres/WebsiteAnalysisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.j4ai.outlook.point3acres;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteAnalysisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
